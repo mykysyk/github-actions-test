a6b5acfa0b81de5f4edb9bbba70f112eceb9f1cd
mykysyk <mykysyk@MacBookPro.local>
2021-09-09 15:00:47 +0900

    first commit

---
+++ src/zip_code_app/core.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+import json
+
+URL = 'http://zipcloud.ibsnet.co.jp/api/search?zipcode={}'
+
+
+def zip_api(zip_code):
+    r = requests.get(URL.format(zip_code))
+    return r.json()
+
+
+def main():
+    zip_code = sys.argv[1]
+    zip_json = zip_api(zip_code)
+    print(json.dumps(zip_json, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    main()
